b3e7f232f1241efa4500832be29e0c6460a7de6e
OwenSyKim <owen.sykim@gmail.com>
2023-06-07 02:03:53 -0700

    Setup all cites' temperature and create Button and Box Components

--- src/components/WeatherBox.js
+++ src/components/WeatherBox.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const WeatherBox = () => {
+const WeatherBox = ({ weather }) => {
+    
+    console.log(weather?.weather)
+// {weather?.name} => 삼항연산식
   return (
-    <div class = "weather-box">
-        <div>Seoul</div>
-        <h2>30c' / 230 F</h2>
+    <div className = "weather-box">
+        <div>{weather?.name}</div> 
+        <h2>{weather?.main.temp}/{weather?.main.temp}</h2>
+        <h3>{weather?.weather[0].description}</h3>
     </div>
   )
 }
